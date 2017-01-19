5d96a2d4b3cdbc7908490bca7521d89d49c17d3b
Ahmed Tarek <ahmed.tokyo1@gmail.com>
2017-01-19 20:42:39 +0200

    Update evaluate-courses.js

--- evaluate-courses.js
+++ evaluate-courses.js
@@ -2,9 +2,9 @@ var grade = 0;
 var attendance = 0;
 var studyTime = 5;
 var amountOfWork = 2;
-var indexString = 1;
 var remark = "";
 // script logic
+var indexString;
 for (var i = 1; i < 20; i++) {
   indexString = (i<10)?'0'+i:i;
   document.getElementById('objRptr_ctl'+indexString+'_grade_'+grade).click();
